c16f2bef8f7195ef44e2181344d8ae555d3114b2
Ashwin <amathur0821@gmail.com>
2026-07-21 19:10:16 +0000

    fix(forms): import useForm from react-hook-form instead of react

--- src/components/forms/CaseSubmitForm.jsx
+++ src/components/forms/CaseSubmitForm.jsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react'
+import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import toast from 'react-hot-toast'
